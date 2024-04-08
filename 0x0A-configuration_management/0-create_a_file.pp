45cb8342fe48ed62acc68f2774d70eeffef74b18
toluolukosi <toluolukosi@gmail.com>
2024-04-08 01:08:39 +0000

    1000th commit sigh

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
-# Create file in /tmp
+# create file in /tmp
 
 file { '/tmp/school' :
-    path    => '/tmp/school',
+    content => 'I love Puppet',
     mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
-    content => 'I love Puppet',
     }
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# install flask from pip3
+#!/usr/bin/pup
+# install a specific version of flask (2.1.0)
 package { 'flask' :
     ensure   => '2.1.0',
-    provider => 'pip3',
-    }
+    provider => 'pip3'
+}
