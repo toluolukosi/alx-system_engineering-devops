37e8fea83df425834a2d2380f8222c272a907c09
root <toluolukosi@gmail.com>
2024-04-10 13:47:38 +0000

    1-install_a_package.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# create file in /tmp
+# creates a file in /tmp
 
-file { '/tmp/school' :
-    content => 'I love Puppet',
-    mode    => '0744',
-    owner   => 'www-data',
-    group   => 'www-data',
-    }
+file { '/tmp/school':
+  content =>'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
